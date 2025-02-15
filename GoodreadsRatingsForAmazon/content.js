409b6230eff40310af50372238497979262000c5
Ruben Martinez <rub3nmv@gmail.com>
2025-02-15 15:14:08 +0100

    Fixes for Audible
    Fixes problems with audible and adds more support for most audible domains.

--- GoodreadsRatingsForAmazon/content.js
+++ GoodreadsRatingsForAmazon/content.js
@@ -1,6 +1,7 @@
 const IS_DEBUG = false;
 const BOOKINFO_RETRIEVE_INTERVAL = 1000;
 const MAX_BOOK_CHECK_TRIES = 15;
+const AUDIBLE_PATTERN = /^https?:\/\/(www\.)?audible\.[^/]+\/pd\//;
 var checkInterval = 0;
 var parser = new DOMParser();
 var isAudibleCom = false;
@@ -47,7 +48,7 @@ function findAsinOrIsbnText() {
 		let reviewsDetail = document.getElementById('detailBullets_averageCustomerReviews');
 		if (reviewsDetail) {
 			found = reviewsDetail.getAttribute('data-asin');
-			if(found === '') found = undefined;
+			if (found === '') found = undefined;
 		}
 	}
 
@@ -77,27 +78,42 @@ function getASIN() {
 	try {
 		// Audible
 		if (isAudibleCom) {
-			log("IsAudible. Searching for ASIN...");
-			// Method 1
+			log("IsAudible. Searching for ASIN with Method 1...");
+			var asinInput = document.getElementById('reviewsAsinForAmazonTab');
+			if (asinInput) {
+				asin.push(asinInput.value);
+				log("asin found: " + asin);
+			}
+			log("IsAudible. Searching for ASIN with Method 2...");
 			var iframe = document.getElementById('adbl-amzn-portlet-reviews');
-			var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-			var stateObject = innerDoc.getElementById('cr-state-object');
-			if (stateObject !== null) {
-				var dataStateString = stateObject.getAttribute('data-state');
-				if (dataStateString !== null) {
-					var dataStateJson = JSON.parse(dataStateString);
-					asin.push(dataStateJson['asin']);
-					log("Method 1 asin found: " + asin);
+			if (iframe) {
+				var innerDoc = iframe.contentDocument || iframe.contentWindow?.document;
+				var stateObject = innerDoc.getElementById('cr-state-object');
+				if (stateObject !== null) {
+					var dataStateString = stateObject.getAttribute('data-state');
+					if (dataStateString !== null) {
+						var dataStateJson = JSON.parse(dataStateString);
+						asin.push(dataStateJson['asin']);
+						log("asin found: " + asin);
+					}
 				}
 			}
-			// Method 2
+
+			log("IsAudible. Searching for ASIN with Method 3...");
 			var asinElement = document.getElementById("reviewsAsinUS");
-			if (asinElement === undefined) asinElement = document.getElementsByName("productAsin")[0];
-			if (asinElement === undefined) {
-				asinElement = document.querySelectorAll("[data-asin]")[0].getAttribute("data-asin");
+			if (!asinElement) {
+				asinElement = document.getElementsByName("productAsin")[0];
 			}
-			asin.push(asinElement === undefined ? false : asinElement.value);
-			log("Method 2 asin found: " + asin);
+			if (!asinElement) {
+				const dataAsinElements = document.querySelectorAll("[data-asin]");
+				if (dataAsinElements.length > 0) {
+					asinElement = dataAsinElements[0].getAttribute("data-asin");
+				}
+			}
+
+			const asinValue = asinElement?.value || asinElement || false;
+			asin.push(asinValue);
+			log("asin found: " + asin);
 		}
 		// Amazon
 		else {
@@ -319,6 +335,69 @@ function processResponse(asin, meta, goodreadsLink, last, uid) {
 	isBusy = false;
 }
 
+function applyStyle(htmlElement) {
+	log("Applying styles to element");
+	try {
+		const styles = `
+		.goodreadsRating {
+			display: block;
+			font-size: 13px !important;
+			line-height: 19px!important;
+			margin-top: 5px !important;
+		}
+
+		.audibleFont {
+			font-family: "Audible Sans", Arial, sans-serif !important;
+			font-size: 12px !important;
+			font-weight: 600 !important;
+			color: #0e5b9b !important;
+		}
+
+		.stars {
+			margin-bottom: 0px !important;
+			margin-right: 5px !important;
+			min-width: 85px !important;
+			height: 20px !important;
+		}
+
+		.staticStars {
+			font-size: 0 !important;
+			background-repeat: no-repeat !important;
+			white-space: nowrap !important;
+			display: inline-block !important;
+			vertical-align: top !important;
+		}
+
+		.staticStar {
+			width: 16px !important;
+			height: 20px !important;
+			float: left !important;
+			background-repeat: no-repeat !important;
+		}
+
+		.staticStar.p0 {
+			background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAABJklEQVR42p2RO4qDUBiFs4NsYZaQeppJLYI2l3EFgtVgL1i4AEHQ2kLFDQipbAQRC6OND/CxhCzhH4/FkIkaSIoPDufBvV4PRLQJx3EnAL3HbqAoigneGjuOM4GXx4yxUxRFBKBfGuu6bqZpSgB6dyyK4vkeSZLOQRBMVVURgIb32ON5/vtgGMbVtm2av2/B8zxKkoS6rgPQ8P5ydLERBOFzOT4Mw+WaeZ5TURTUNA0NwwCg4S0ZOuiuvnk2hfmkGwbjOP4DHjJ09h4Mv+erbdvVGB6yp69tWZZZluVqDA/Z07Hrule8MK7Z9/0EoOEh2xvjl31cLheq65qyLDNlWT4CaHjI0Nkcq6r6E8fxzfd94fEEeMjQ2RxrmqYzxo7QWyBD5977Bcdse9huaoxSAAAAAElFTkSuQmCC") !important;
+		}
+
+		.staticStar.p3 {
+			background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAABaklEQVQoz2NgwAFsbGwMQJiBHBAeHt4PwmRpbmlpuQ/CJGt0cXExWLZs2X8QBrFJ0pyTk9O/a9eu/yAMYuNUuNbQ3OGKoQUKnjp16v3jx4//B2EQ283NzQEdgAOzW9/o/HZdo/+XdRB427Zt/y9cuADGIPbkyZP/A/0Pxs3Nzf8LCgrOAzXLg22v0TXoX6al9/+cms7/S0B87ty5/1evXgVjEPvw4cP/9+3bB/bG9OnTMb2RqqvvP19d8/1FRdX/165dQ8EgQ4CueA/U6I/T/+1qGvYXpOUxNJ8/f/5/U1OTPd5Q3i0l039eTBJD89GjR/83NDTgTzDnBIXPAzHYmZcvX74PwiA2KNT7+/vP49SYpK6ucJKH9/9iMfH/u3fv7g8KCuIHYRD7zJkz/1euXPnf3t5eAavmqRKS+RMlpd6nqKlhBMqUKVP8N2zY8D4uLi4fq+ZSRcV6BwMDfmNjY1xJlj8zM7MeWQwAhBL+K9l+SwUAAAAASUVORK5CYII=") !important;
+		}
+
+		.staticStar.p6 {
+			background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAABR0lEQVQoz2NgwAFsbGzAmCxwytWvH4SNjY0ZQJgkcN3R+z4Ik6y5wdza4JqN638QBrFJ0nzczLb/qpndfxAGsXFqXmto7nDF0AId3wfi/1AMYjugA2BgGjB06xud365r9P+yDn7c0tICxs3Nzf8LCgrOAzXLg22v0TXoX6al9/+cms7/Szjwvn37/u/atev/9OnT+zGcn6qr7z9fXfP9RUXV/9jwhQsX3gM1+uMMqHY1DfsL0vL/seGmpiZ7vKG8W0qm/7yY5H8QPi4uBcYwfkNDQz9ezecEhc8D8f/N4pL/++UV7oMwiA0S6+/vP49TY5K6usJJHt7/i8XE/1cpKvY7GBjwgzCIDRJbuXLlf3t7ewWsmqdKSOZPlJR6n6KmhhIooAQCEtuwYcP7uLi4fKyaSxUV60E2oYvD0raLiwt/ZmZmPbIcALuzziYfrXcdAAAAAElFTkSuQmCC") !important;
+		}
+
+		.staticStar.p10 {
+			background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAABIUlEQVQoz2NgwAEa7V0MQBhd3NjYGI5xglOufv0gTJbm647e90GYZM0N5tYG12xc/4MwiE2S5uNmtv1Xzez+gzCIjVPzWkNzhyuGFuj4PhD/h+L76PIgPbFGJgYM3fpG57frGv2/rEMcBqkF6fE1MpYHO6VG16B/mZbe/3NqOv8v4cAgOZAakFoMP6fq6vvPV9d8f1FR9T82DJIDqcEZYO1qGvYXpOX/Y8MgObyhvVtKpv+8mOR/ED4uLgXGMD5IDq/mc4LC54H4/2Zxyf/98gr3QRjEBomB5HBqTlJXVzjJw/t/sZj4/ypFxX4HAwN+EAaxQWIgOZAarJqnSkjmT5SUep+ipuaPnmhAYiA5kBqsmksVFetBNuFK6yA5kBpkzQAvHdWbf69jygAAAABJRU5ErkJggg==") !important;
+		}`;
+		const styleElement = document.createElement('style');
+		styleElement.textContent = styles;
+		htmlElement.appendChild(styleElement);
+	} catch (error) {
+		logex(applyStyle, "Error applying styles to element" + error);
+	}
+}
+
 /**
  * Get book reviews from ASIN number and show rating
  * last = boolean. Checks if is the last recursive pass
@@ -338,11 +417,11 @@ function retrieveBookInfo(asin, last) {
 			// log(data);
 			// GET RATINGS INFO
 			let meta = doc.querySelectorAll("#bookMeta");
-			
+
 			if (!meta || meta.length === 0) {
 				log("bookMeta not found, searching for .BookPageMetadataSection__ratingStats", uid);
 				meta = doc.querySelectorAll(".BookPageMetadataSection__ratingStats");
-				
+
 				if (!meta || meta.length === 0) {
 					log(".BookPageMetadataSection__ratingStats not found, searching for .BookPageMetadataSection", uid);
 					meta = doc.querySelectorAll(".BookPageMetadataSection");
@@ -364,14 +443,59 @@ function retrieveBookInfo(asin, last) {
 	});
 }
 
+function findStarRatingTag(element = document) {
+	// First, check if the element contains the adbl-star-rating tag
+	const productMetadata = element.querySelector("adbl-star-rating");
+
+	// If the tag is found, return it
+	if (productMetadata) {
+		return productMetadata;
+	}
+
+	// If the element has a shadow root, search inside it recursively
+	const shadowRoots = element.shadowRoot ? [element.shadowRoot] : [];
+
+	// Also look for shadow roots in any child elements
+	element.querySelectorAll("*").forEach(child => {
+		if (child.shadowRoot) {
+			shadowRoots.push(child.shadowRoot);
+		}
+	});
+
+	// Recursively search all shadow roots
+	for (let shadowRoot of shadowRoots) {
+		const foundInShadow = findStarRatingTag(shadowRoot);
+		if (foundInShadow) {
+			return foundInShadow;
+		}
+	}
+
+	// If not found in this element or any shadow roots, return null
+	return null;
+}
+
 function AppendToAudible(contentSpan) {
 	if (finished) return;
 	log("AppendToAudible");
-	var ratingsLabel = document.getElementsByClassName("ratingsLabel");
-	if (ratingsLabel.length > 0) {
-		var parentUl = ratingsLabel[0].parentNode;
-		parentUl.insertBefore(contentSpan, ratingsLabel[0]);
+
+	const ratingsLabel = findStarRatingTag();
+
+	if (ratingsLabel) {
+		console.log("Found adbl-star-rating:", ratingsLabel);
+		const parentUl = ratingsLabel.parentNode;
+		applyStyle(parentUl);
+		// Insert contentSpan after ratingsLabel
+		const nextSibling = ratingsLabel.nextSibling;
+		if (nextSibling) {
+			parentUl.insertBefore(contentSpan, nextSibling);  // Insert before the next sibling
+		} else {
+			// If there's no next sibling, append it at the end
+			parentUl.appendChild(contentSpan);
+		}
+
 		finished = true;
+	} else {
+		console.log("adbl-star-rating not found");
 	}
 }
 /**
@@ -447,13 +571,13 @@ function SelectById(idArray) {
 function checkIfBook() {
 	log("checkIfBook attempt " + checkInterval);
 	// Audible
-	if (isAudibleCom) return window.location.href.indexOf("audible.com/pd") > 0;
+	if (isAudibleCom) return AUDIBLE_PATTERN.test(window.location.href);
 	// Amazon
-	var bookDetectionIdArray = ["ebooksImageBlockOuter", "booksTitle", "bookEdition", 
-								"pBookUpsellBorrowButton", "booksImageBlock_feature_div", 
-								"pbooksReadSampleButton-announce", "rpi-attribute-book_details-fiona_pages", 
-								"rpi-attribute-book_details-customer_recommended_age", "rpi-attribute-book_details-isbn13",
-								"bookslegalcompliancebanner_feature_div"];
+	var bookDetectionIdArray = ["ebooksImageBlockOuter", "booksTitle", "bookEdition",
+		"pBookUpsellBorrowButton", "booksImageBlock_feature_div",
+		"pbooksReadSampleButton-announce", "rpi-attribute-book_details-fiona_pages",
+		"rpi-attribute-book_details-customer_recommended_age", "rpi-attribute-book_details-isbn13",
+		"bookslegalcompliancebanner_feature_div"];
 	return SelectById(bookDetectionIdArray) !== null;
 }
 
@@ -483,7 +607,7 @@ var isBookChecker = window.setInterval(function () {
 	}
 	checkInterval++;
 	// Check if the domain is Amazon or Audible
-	isAudibleCom = window.location.hostname.indexOf("audible.com") > 0;
+	isAudibleCom = AUDIBLE_PATTERN.test(window.location.href);
 	log("isAudibleCom = " + isAudibleCom);
 	if (checkIfBook()) {
 		window.clearInterval(isBookChecker);
@@ -495,25 +619,36 @@ var isBookChecker = window.setInterval(function () {
 		baseAPI.storage.local.get(options, function (items) {
 			displayAmazonGoodreads = items.displayAmazonGoodreads;
 		});
-
+		var asinList = [];
 		// Try to retrieve info about the book from the asin codes found
 		var metaInfoChecker = window.setInterval(function () {
+			checkInterval++;
+			if (checkInterval > MAX_BOOK_CHECK_TRIES) {
+				window.clearInterval(metaInfoChecker);
+				return;
+			}
+
 			if (isBusy) {
 				log("Still busy, wait...");
 				return;
 			}
-			
+
 			asinCheckedList = [];
-			
+
 			goodreadsRetrieveInternal++;
 			log("Interval number " + goodreadsRetrieveInternal);
 
-			var asinList = getASIN();
+			if (!asinList || asinList.length === 0) {
+				log("No ASIN found, searching");
+				asinList = getASIN();
+			}
 			if (asinList && asinList.length > 0) {
+				log("Finished: " + finished + ", goodreadsRetrieveInternal: " + goodreadsRetrieveInternal);
 				if (finished || goodreadsRetrieveInternal > 20) {
 					log("asin length is " + asinList.length + ", finished = " + finished);
 					window.clearInterval(metaInfoChecker);
 				} else {
+					log("Not finished yet, continue retrieving book info");
 					for (let index = 0; index < asinList.length; index++) {
 						let currentAsin = asinList[index];
 						// Search in checked asin list
@@ -532,6 +667,9 @@ var isBookChecker = window.setInterval(function () {
 						}
 					}
 				}
+			}
+			else {
+				log("No ASIN found");
 			}
 		}, BOOKINFO_RETRIEVE_INTERVAL);
 
